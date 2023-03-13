ba021679dba7d105c900ce750df213858b35830a
richarddddd198 <63178095+richarddddd198@users.noreply.github.com>
2023-03-13 09:20:40 +0800

    conmite

---
+++ README.md
@@ -0,0 +1,247 @@
+# roLabelImgV2
+
+### 1.Installation
+
+roLabelImgV2 is a graphical image annotation tool can label ROTATED rectangle regions, which is rewrite from 'roLabelImg'.
+
+The original version 'roLabelImg''s link is here https://github.com/cgvict/roLabelImg.
+
+### 2.New functions
+
+1. **The<segmentation>tag is added to the XML file to save the rectangular coordinates**
+
+2. **RoLabelImgV2 adds the function of one-button conversion of COCO format(Xml→Json)**
+
+### 3.How to use
+
+1. Refer to the introduction document of roLabelImg
+
+2. Windows users can download and extract the **roLabelImgV2.zip** file and run it directly
+
+
+
+
+
+### ↓↓↓↓↓↓↓↓↓Original document↓↓↓↓↓↓↓↓↓
+
+# roLabelImg
+
+roLabelImg is a graphical image annotation tool can label ROTATED
+rectangle regions, which is rewrite from \'labelImg\'.
+
+The original version \'labelImg\'\'s link is
+here\<<https://github.com/tzutalin/labelImg>\>.
+
+It is written in Python and uses Qt for its graphical interface.
+
+[Watch a demo by author cgvict]{.title-ref}
+
+![Demo Image](https://raw.githubusercontent.com/cgvict/roLabelImg/master/demo/demo4.png)
+
+![image](https://raw.githubusercontent.com/cgvict/roLabelImg/master/demo/demo_v2.5.gif)
+
+<https://youtu.be/7D5lvol_QRA>
+
+Annotations are saved as XML files almost like PASCAL VOC format, the
+format used by [ImageNet](http://www.image-net.org/).
+
+## XML Format
+
+```
+<annotation verified="yes">
+  <folder>hsrc</folder>
+  <filename>100000001</filename>
+  <path>/Users/haoyou/Library/Mobile Documents/com~apple~CloudDocs/OneDrive/hsrc/100000001.bmp</path>
+  <source>
+    <database>Unknown</database>
+  </source>
+  <size>
+    <width>1166</width>
+    <height>753</height>
+    <depth>3</depth>
+  </size>
+  <segmented>0</segmented>
+  <object>
+    <type>bndbox</type>
+    <name>ship</name>
+    <pose>Unspecified</pose>
+    <truncated>0</truncated>
+    <difficult>0</difficult>
+    <bndbox>
+      <xmin>178</xmin>
+      <ymin>246</ymin>
+      <xmax>974</xmax>
+      <ymax>504</ymax>
+    </bndbox>
+  </object>
+  <object>
+    <type>robndbox</type>
+    <name>ship</name>
+    <pose>Unspecified</pose>
+    <truncated>0</truncated>
+    <difficult>0</difficult>
+    <robndbox>
+      <cx>580.7887</cx>
+      <cy>343.2913</cy>
+      <w>775.0449</w>
+      <h>170.2159</h>
+      <angle>2.889813</angle>
+    </robndbox>
+    <segmentation>
+      <x1>485.7773</x1>
+      <y1>621.7875</y1>
+      <x2>381.3207</x2>
+      <y2>709.6281</y2>
+      <x3>338.6755</x3>
+      <y3>658.9161</y3>
+      <x4>443.1321</x4>
+      <y4>571.0755</y4>
+    </segmentation>
+  </object>
+</annotation>
+```
+
+## Installation
+
+### Download prebuilt binaries of original \'labelImg\'
+
+- [Windows & Linux](http://tzutalin.github.io/labelImg/)
+- OS X. Binaries for OS X are not yet available. Help would be
+  appreciated. At present, it must be [built from source](#os-x).
+
+### Build from source
+
+Linux/Ubuntu/Mac requires at least [Python
+2.6](http://www.python.org/getit/) and has been tested with [PyQt
+4.8](http://www.riverbankcomputing.co.uk/software/pyqt/intro).
+
+#### Ubuntu Linux
+
+```
+sudo apt-get install pyqt4-dev-tools
+sudo pip install lxml
+make all
+./roLabelImg.py
+./roLabelImg.py [IMAGE_PATH] [PRE-DEFINED CLASS FILE]
+```
+
+#### OS X
+
+```
+brew install qt qt4
+brew install libxml2
+make all
+./roLabelImg.py
+./roLabelImg.py [IMAGE_PATH] [PRE-DEFINED CLASS FILE]
+```
+
+#### Windows
+
+Download and setup [Python 2.6 or
+later](https://www.python.org/downloads/windows/),
+[PyQt4](https://www.riverbankcomputing.com/software/pyqt/download) and
+[install lxml](http://lxml.de/installation.html).
+
+Open cmd and go to [roLabelImg](#roLabelimg) directory
+
+```
+pyrcc4 -o resources.py resources.qrc
+python roLabelImg.py
+python roLabelImg.py [IMAGE_PATH] [PRE-DEFINED CLASS FILE]
+```
+
+### Use Docker
+
+```
+docker pull tzutalin/py2qt4
+
+docker run -it \
+--user $(id -u) \
+-e DISPLAY=unix$DISPLAY \
+--workdir=$(pwd) \
+--volume="/home/$USER:/home/$USER" \
+--volume="/etc/group:/etc/group:ro" \
+--volume="/etc/passwd:/etc/passwd:ro" \
+--volume="/etc/shadow:/etc/shadow:ro" \
+--volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
+-v /tmp/.X11-unix:/tmp/.X11-unix \
+tzutalin/py2qt4
+```
+
+You can pull the image which has all of the installed and required
+dependencies.
+
+## Usage
+
+### Steps
+
+1. Build and launch using the instructions above.
+2. Click \'Change default saved annotation folder\' in Menu/File
+3. Click \'Open Dir\'
+4. Click \'Create RectBox\'
+5. Click and release left mouse to select a region to annotate the rect
+   box
+6. You can use right mouse to drag the rect box to copy or move it
+
+The annotation will be saved to the folder you specify.
+
+You can refer to the below hotkeys to speed up your workflow.
+
+### Create pre-defined classes
+
+You can edit the
+[data/predefined_classes.txt](https://github.com/tzutalin/labelImg/blob/master/data/predefined_classes.txt)
+to load pre-defined classes
+
+### Hotkeys
+
+  ------------ --------------------------------------------
+
+  Ctrl + u     Load all of the images from a directory
+
+  Ctrl + r     Change the default annotation target dir
+
+  Ctrl + s     Save
+
+  Ctrl + d     Copy the current label and rect box
+
+  Space        Flag the current image as verified
+
+  w            Create a rect box
+
+  e            Create a Rotated rect box
+
+  d            Next image
+
+  a            Previous image
+
+  r            Hidden/Show Rotated Rect boxes
+
+  n            Hidden/Show Normal Rect boxes
+
+  del          Delete the selected rect box
+
+  Ctrl++       Zoom in
+
+  Ctrl\--      Zoom out
+
+  ↑→↓←         Keyboard arrows to move selected rect box
+
+  zxcv         Keyboard to rotate selected rect box
+
+  ------------ --------------------------------------------
+
+### How to contribute
+
+Send a pull request
+
+### License
+
+[Free software: MIT
+license](https://github.com/cgvict/roLabelImg/blob/master/LICENSE)
+
+### Related
+
+1. [ImageNet Utils](https://github.com/tzutalin/ImageNet_Utils) to
+   download image, create a label text for machine learning, etc
+2. [Docker hub to run it](https://hub.docker.com/r/tzutalin/py2qt4)
---
+++ libs/convertCoco.py
@@ -0,0 +1,144 @@
+import json
+import xml.etree.ElementTree as ET
+import os
+import glob
+import random
+import shutil
+class ConvertCoCo(object):
+
+    START_BOUNDING_BOX_ID = 1
+
+    def __init__(self, imgdir,savedir):
+        self.imgdir = imgdir
+        self.savedir = savedir
+
+
+    def get(self,root, name):
+        vars = root.findall(name)
+        return vars
+
+    def get_and_check(self,root, name, length):
+        vars = root.findall(name)
+        if len(vars) == 0:
+            raise NotImplementedError('Can not find %s in %s.'%(name, root.tag))
+        if length > 0 and len(vars) != length:
+            raise NotImplementedError('The size of %s is supposed to be %d, but is %d.'%(name, length, len(vars)))
+        if length == 1:
+            vars = vars[0]
+        return vars
+
+
+    def get_filename_as_int(self,filename):
+        try:
+            filename = os.path.splitext(filename)[0]
+            return int(filename)
+        except:
+            raise NotImplementedError('Filename %s is supposed to be an integer.'%(filename))
+
+    def split_data(self,data,ratio):
+        random.seed(20230312)
+        random.shuffle(data)
+        nrow = len(data)
+        offset  = int(nrow * ratio)
+        if offset != nrow:
+            train_data = data[:offset]
+            val_data = data[offset:]
+            return {'train':train_data,'valid':val_data}
+        else:
+            return {'train':data,'valid':[]}
+        
+
+    def create_dir(self,path):
+        if not os.path.exists(path):
+            os.mkdir(path)
+
+    def convert(self,ratio):
+        """
+
+        :param xml_list: 由xml文件名组成的列表['1.xml','2.xml']
+        :param xml_dir:  原始存放xml文件夹路径
+        :param json_file: 生成json文件的存放路径
+        :return:
+        """
+
+        xml_f_list = list(glob.glob(self.savedir+"/*.xml"))
+
+        xml_f_dict = self.split_data(xml_f_list,ratio)
+        for key,data in xml_f_dict.items():
+            if data:
+                json_dict = {"images":[], "type": "instances", "annotations": [],
+                            "categories": []}
+                categories = {}
+                bnd_id = self.START_BOUNDING_BOX_ID
+                annotations_dir = os.path.join(os.path.dirname(self.imgdir),"annotations")#创建保存json文件的文件夹
+                images_dir = os.path.join(os.path.dirname(self.imgdir),"images")#创建保存图像文件的文件夹
+                self.create_dir(annotations_dir)
+                self.create_dir(images_dir)
+                save_json_path = os.path.join(annotations_dir,key+'.json')#创建保存json文件
+                
+                for xml_f in data:
+                    base_name = os.path.basename(xml_f).split('.')[0]
+                    scr_img_path = glob.glob(os.path.join(self.imgdir,base_name)+'.*')
+                    scr_img_path  = scr_img_path[0]
+                    dist_img_path = os.path.join(images_dir,os.path.basename(scr_img_path))
+                    shutil.copy(scr_img_path,dist_img_path)#复制图片到images文件夹中
+
+                    tree = ET.parse(xml_f)
+                    root = tree.getroot()
+                    img_path = self.get(root, 'path')
+                    if len(img_path) == 1:
+                        filename = os.path.basename(img_path[0].text)
+                    elif len(img_path) == 0:
+                        filename = self.get_and_check(root, 'filename', 1).text
+                    else:
+                        raise NotImplementedError('%d paths found in %s'%(len(img_path), img_path))
+                    ## The filename must be a number
+                    image_id = self.get_filename_as_int(filename)
+                    size = self.get_and_check(root, 'size', 1)
+                    width = int(self.get_and_check(size, 'width', 1).text)
+                    height = int(self.get_and_check(size, 'height', 1).text)
+                    image = {'file_name': filename, 'height': height, 'width': width,
+                            'id':image_id}
+                    json_dict['images'].append(image)
+
+                    for obj in self.get(root, 'object'):
+                        category = self.get_and_check(obj, 'name', 1).text
+                        if category not in categories:
+                            new_id = len(categories) +1
+                            categories[category] = new_id
+                        category_id = categories[category]
+                        bndbox = self.get_and_check(obj, 'robndbox', 1)
+                        cx = float(self.get_and_check(bndbox, 'cx', 1).text)
+                        cy = float(self.get_and_check(bndbox, 'cy', 1).text)
+                        w = float(self.get_and_check(bndbox, 'w', 1).text)
+                        h = float(self.get_and_check(bndbox, 'h', 1).text)
+                        #angle = float(self.get_and_check(bndbox,'angle',1).text) 
+                        
+                        segmentation = self.get_and_check(obj,'segmentation',1)
+                        x1 = float(self.get_and_check(segmentation,'x1',1).text)
+                        y1 = float(self.get_and_check(segmentation,'y1',1).text)
+                        x2 = float(self.get_and_check(segmentation,'x2',1).text)
+                        y2 = float(self.get_and_check(segmentation,'y2',1).text)
+                        x3 = float(self.get_and_check(segmentation,'x3',1).text)
+                        y3 = float(self.get_and_check(segmentation,'y3',1).text)
+                        x4 = float(self.get_and_check(segmentation,'x4',1).text)
+                        y4 = float(self.get_and_check(segmentation,'y4',1).text)
+
+                        ann = {'area': w*h, 'iscrowd': 0, 'image_id':
+                            image_id, 'bbox':[cx, cy, w, h],
+                            'category_id': category_id, 'id': bnd_id, 'ignore': 0,
+                            'segmentation': [x1,y1,x2,y2,x3,y3,x4,y4]}
+                        json_dict['annotations'].append(ann)
+                        bnd_id = bnd_id + 1
+
+                for cate, cid in categories.items():
+                    cat = {'supercategory': 'none', 'id': cid, 'name': cate}
+                    json_dict['categories'].append(cat)
+
+
+                json_fp = open(save_json_path, 'w')
+                json_str = json.dumps(json_dict,ensure_ascii=False, indent=4)
+                json_fp.write(json_str)
+                json_fp.close()
+                print("Done")
+
